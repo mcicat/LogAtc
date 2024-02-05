71db59a361945e5b8cc4dfc6fbaf7b16120bdf77
mcicat <michalcicatka@seznam.cz>
2024-02-05 13:51:02 +0100

    Added intial version of the project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogAtc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
